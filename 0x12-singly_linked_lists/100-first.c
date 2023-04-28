27867c088de55402e6d63528fbc00eb8859943fd
2152001 <tokaabdelgawad60@gmail.com>
2023-04-29 02:00:03 +0300

    done

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-/**
- * premain - function executed before main
- * Return: no return.
- */
 
-void __attribute__ ((constructor)) premain()
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
 {
-printf("You're beat! and yet, you must allow");
-printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
+
 ~
 ~
 ~
