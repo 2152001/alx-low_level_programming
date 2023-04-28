9c0a6f5ade539c8f29baac5773af21c2f3f03199
2152001 <tokaabdelgawad60@gmail.com>
2023-04-29 01:56:17 +0300

    done

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * premain - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+printf("You're beat! and yet, you must allow");
+printf(",\nI bore my house upon my back!\n");
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+100-first.c [unix] (01:59 01/01/1970)                                  0,0-1 All
+"100-first.c" [New]
+
